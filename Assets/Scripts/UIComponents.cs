7a8b64ca4168fa61377369e2a28b34426e1c2ef4
Umer <asgharumer219@gmail.com>
2021-10-25 04:37:50 -0700

    Files added

---
+++ Assets/Scripts/UIComponents.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+[Serializable]
+public class UIComponents 
+{
+    [Header ("Buttons")]
+    public Button bttn;
+    [Header("Images")]
+    public Image img;
+    [Header("Input")]
+    public InputField input;
+    [Header("Text")]
+    public Text txt;
+    [Header("Scroll")]
+    public ScrollRect scroll;
+    [Header("Slider")]
+    public Slider slider;
+    [Header("Toggle")]
+    public Toggle toggle;
+}
